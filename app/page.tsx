78822f3a6b28ad030427142843d6ff6858c1b262
dongmuel02 <kdh200022@gmail.com>
2025-11-09 02:50:10 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import TipJarApp from '@/components/TipJarApp';
+
 export default function Home() {
   return (
-    <div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Tip Jar App - 학번: [92113504] 이름: [김동현]</h1>
-      {/* 나머지 UI 컴포넌트 추가 */}
-    </div>
+      <TipJarApp />
+    </main>
   );
 }
